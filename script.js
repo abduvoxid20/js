8cb39e825d2dfe100cddf7dea2e9ef175f55821e
elik <elik@example.com>
2021-04-27 15:35:36 +0500

    js

---
+++ script.js
@@ -0,0 +1,12 @@
+var shark = 3, odam = 1
+var dolphin = +prompt('Delfin bomi 1/0=?')
+odam = dolphin + odam
+
+var s1 = +prompt("Odamdan qirg'oqgacha masofa ")
+var s2 = +prompt("Akuladan odamgacha masofa ")
+
+if ((s1+s2) / shark > s1 / odam) {
+    alert('A live !')
+} else {
+    alert('Shark bait')
+}
